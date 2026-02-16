f4eb5a773b7e8eb958de304275553ba23c16441d
Tonyvirues <tonimavir@gmail.com>
2026-02-16 20:09:10 +0100

    ajustes finalizados

--- Proyecto PHP/listado.php
+++ Proyecto PHP/listado.php
@@ -27,7 +27,7 @@ $resultado = $connect->query($obtenerTodosCv);
     <header>
         <!--Cabecera-->
         <div class="navbar navbar-dark bg-primary mb-4">
-            <div class="container-fluid">              
+            <div class="container-fluid">
                 <button class="btn">
                     <a class="nav-link navbar-brand mb-0 h1" href="index.html">
                         <i class="bi bi-file-earmark-person"></i>Creador Curriculums</a>
@@ -35,45 +35,81 @@ $resultado = $connect->query($obtenerTodosCv);
             </div>
         </div>
     </header>
-
-    <div class="container-fluid mt-5">
-
+    <main class="mb-3">
         <h2 class=" text-info mb-4 ms-4">Curriculums almacenados</h2>
-        <div class="row">
 
-            <!--Menú de navegación.-->
-            <div class="col-md-3">
-                <div class="px-3 py-4">
+        <div class="container-fluid mt-5">
+            <div class="row">
 
-                    <nav class=" rounded-4 shadow-sm p-3 h-100" style="background-color: #e3f2fd;">
+                <!--Menú de navegación.-->
+                <div class="col-md-3">
+                    <div class="px-3 py-4">
 
-                        <ul class="nav nav-pills flex-column gap-1">
-                            <li class="nav-item">
-                                <a class="nav-link " href="index.html">Creación Curriculum</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="listado.php">Mis curriculums</a>
-                            </li>
-                        </ul>
+                        <nav class=" rounded-4 shadow-sm p-3 h-100" style="background-color: #e3f2fd;">
 
-                    </nav>
+                            <ul class="nav nav-pills flex-column gap-1">
+                                <li class="nav-item">
+                                    <a class="nav-link " href="index.html">Creación Curriculum</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="listado.php">Mis curriculums</a>
+                                </li>
+                            </ul>
 
+                        </nav>
+
+                    </div>
                 </div>
-            </div>
 
-            <!--Colocamos la tabla a la derecha del menú.-->
-            <div class="col-md-9">
-
-                <!--Condicional if, que imprime la tabla con registros
+                <!--Colocamos la tabla a la derecha del menú.-->
+                <div class="col-md-9">
+                    <div class="col-lg-10 col-xl-9 mx-auto">
+                        <!--Condicional if, que imprime la tabla con registros
                     en caso de no haber registro imprime una tabla vacía.-->
-                <?php if ($resultado->num_rows > 0): ?>
+                        <?php if ($resultado->num_rows > 0): ?>
 
-                    <!--Tabla de curriculums.-->
-                    <div class="col-9">
+                            <!--Tabla de curriculums.-->
 
-                        <div class="py-4 me-5">
 
-                            <table class="table table-striped table-bordered rounded-2">
+                            <div class="py-4 me-5">
+
+                                <table class="table table-striped table-bordered rounded-2 mt-2">
+                                    <thead class="table-primary">
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Email</th>
+                                            <th>Fecha creación</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+
+                                        <!--Código php, que introduce los datos recodigos.-->
+                                        <?php while ($cv = $resultado->fetch_assoc()): ?>
+
+                                            <tr>
+                                                <td><?= htmlspecialchars($cv['nombre']) ?> <?= htmlspecialchars($cv['apellidos']) ?></td>
+                                                <td><?= htmlspecialchars($cv['email']) ?></td>
+                                                <td><?= $cv['fecha_creacion'] ?></td>
+                                                <td>
+                                                    <a href="visualizar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm  btn-secondary">Ver</a>
+                                                    <a href="editar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm btn-info">Editar</a>
+                                                    <a href="eliminar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm btn-outline-danger" onclick="return confirm('¿Seguro que quieres eliminar esta versión del CV?');">Eliminar</a>
+                                                </td>
+                                            </tr>
+
+                                        <?php endwhile; ?>
+                                    </tbody>
+                                </table>
+
+                            </div>
+
+
+
+
+                        <?php else: ?>
+                            <table class="table table-striped table-bordered rounded-2 mt-4">
                                 <thead class="table-primary">
                                     <tr>
                                         <th>Nombre</th>
@@ -84,69 +120,31 @@ $resultado = $connect->query($obtenerTodosCv);
                                 </thead>
 
                                 <tbody>
+                                    <tr>
+                                        <td>No hay registros</td>
+                                        <td>No hay registros </td>
+                                        <td>No hay registros</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-info" disabled>No hay acciones</button>
 
-                                    <!--Código php, que introduce los datos recodigos.-->
-                                    <?php while ($cv = $resultado->fetch_assoc()): ?>
-
-                                        <tr>
-                                            <td><?= htmlspecialchars($cv['nombre']) ?> <?= htmlspecialchars($cv['apellidos']) ?></td>
-                                            <td><?= htmlspecialchars($cv['email']) ?></td>
-                                            <td><?= $cv['fecha_creacion'] ?></td>
-                                            <td>
-                                                <a href="visualizar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm btn-primary">Ver</a>
-                                                <a href="editar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
-                                                <a href="eliminar_cv.php?id=<?= $cv['id'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('¿Seguro que quieres eliminar esta versión del CV?');">Eliminar</a>
-                                            </td>
-                                        </tr>
-
-                                    <?php endwhile; ?>
+                                        </td>
+                                    </tr>
                                 </tbody>
                             </table>
 
-                        </div>
+                        <?php endif; ?>
 
                     </div>
+                    <!--Fin del div de la parte derecha-->
+                </div>
 
-
-                <?php else: ?>
-                    <p class="text-info mb-4 ms-5">NO HAY CV GUARDADOS</p>
-
-                    <table class="table table-striped table-bordered rounded-2">
-                        <thead class="table-primary">
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Email</th>
-                                <th>Fecha creación</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr>
-                                <td>No hay registros</td>
-                                <td>No hay registros </td>
-                                <td>No hay registros</td>
-                                <td>
-                                    <button class="btn btn-sm btn-info" disabled>No hay acciones</button>
-
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                <?php endif; ?>
-
-
-            <!--Fin del div de la parte derecha-->
+                <!--aqui cierar el row, antes del container-->
             </div>
 
-        <!--aqui cierar el row, antes del container-->
-        </div>
 
 
-
-    </div><!--aqui cierra el div principal -->
-
+        </div><!--aqui cierra el div principal -->
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
